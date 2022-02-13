05381844092ebd9616db15234c8d13bb2bbe855d
Catherina Govorova <ei.govorova@yandex.ru>
2022-02-13 16:39:15 +0300

    first

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -147,15 +147,3 @@ def profile_unfollow(request, username):
     get_object_or_404(Follow, user=request.user,
                       author__username=username).delete()
     return redirect('posts:profile', username=username)
-
-
-def page_not_found(request, exception):
-    return render(request, 'core/404.html', {'path': request.path}, status=404)
-
-
-def server_error(request, exception):
-    return render(request, 'core/500.html', {'path': request.path}, status=500)
-
-
-def csrf_failure(request, reason=''):
-    return render(request, 'core/403csrf.html')
